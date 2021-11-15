e76a123bf8376dcb9200ab769f5146ce6451ca4d
Alan Matthews <matthewsa1320@gmail.com>
2021-11-14 23:42:45 -0500

    Add model smoke tests.

--- spec/models/herbivore_spec.rb
+++ spec/models/herbivore_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Herbivore, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'valid dinosaur' do
+      let(:cage) { Cage.create }
+      let(:dinosaur) { Dinosaur.create(name: 'Dino', cage: cage) }
+      subject { described_class.new(dinosaur: dinosaur) }
+
+      it 'creates a herbivore instance' do
+        expect(subject.save).to eq true
+      end
+
+      it 'creates a herbivore instance' do
+        expect { subject.save }.to change { Herbivore.count }.by 1
+      end
+    end
+
+    context 'invlaid dinosaur' do
+      let(:cage) { Cage.create }
+      let(:dinosaur) { Dinosaur.create(cage: cage) }
+      subject { described_class.new(dinosaur: dinosaur) }
+
+      it 'doesn create a herbivore instance' do
+        expect(subject.save).to eq false
+      end
+
+      it 'does not change herbivore instance' do
+        expect { subject.save }.to_not change { Herbivore.count }
+      end
+    end
+  end
 end
